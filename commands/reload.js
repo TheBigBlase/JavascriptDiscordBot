bc2e0554cd89c3d4a4160fef155c4967cae73a50
thebigblase <thebigblase@gmail.com>
2019-02-03 13:23:01 +0000

    Working and stable (I hope)

---
+++ commands/reload.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js');
+const settings = require('../settings.json');
+
+module.exports= {
+  Reload: async (message, client) => {
+    console.log("restart triggered");
+    await message.channel.send("I\'ll restart");
+    await client.destroy().then(()=> client.login(settings.token));
+    await message.channel.send("I restarted");
+    console.log("I restarted. All is fine for now.");
+  }
+};
