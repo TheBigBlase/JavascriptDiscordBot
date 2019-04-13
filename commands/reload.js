9e009d7997db19642997d1ad0b9d29558c64228c
Maxime paulin <fanzzy@live.fr>
2019-04-13 11:14:19 +0200

    Reload should work, but only on linux

--- commands/reload.js
+++ commands/reload.js
@@ -1,8 +1,8 @@
 const Discord = require('discord.js');
 const settings = require('../settings.json');
 const chalk = require('chalk');
+const fs = require('fs');
 //const resolve = require ('resolve');
-//const fs = require('fs');
 
 exports.run = async (message,client,args,terminal,clientCommands) => {
 
@@ -11,7 +11,10 @@ exports.run = async (message,client,args,terminal,clientCommands) => {
 
           if(args.isEmpty|| args.lenght >1) return console.log(chalk.red("no argument"));
         let commandName = args[0];
-          if (!clientCommands.has(commandName)) return console.log(chalk.red("No such command to reload"));
+
+        await CheckModule(commandName);
+
+
         console.log(chalk.blue('checking file : '+ commandName));
         await delete require.cache[require.resolve(`./${commandName}.js`)];
         await clientCommands.delete(commandName);
@@ -38,24 +41,25 @@ exports.run = async (message,client,args,terminal,clientCommands) => {
   }
   };
 
-  exports.help={
-    name:"reload"
-  };
 
 
-  /*ReloadTerminal: async (client, args) => {
-    try{
-      if(args.length!=1) return console.log(chalk.red(`Need 1 argument. Number given :${args.lenght}`));
-      let commandName=args[0];
+async function CheckModule(commandName){
+    try {
+      await fs.copyFile(`./commands/${commandName}.js`, `./commands/${commandName}Copy.js`, (err)=>{
+      if (err) console.error(chalk.bgRed("Error while copying : ",err));
+    });
+      console.log(chalk.green(`${commandName} was copied to ${commandName}Copy`));
 
-      if (!client.commands.has(commandName)) return console.log(chalk.bgRed("No such command"));
-      delete require.cache[require.resolve(`./${commandName}.js`)];
-       client.commands.delete(commandName);
-       const props = require(`./${commandName}.js`);
-       client.commands.set(commandName, props);
-      console.log(chalk.green("Reloaded"));
-  }
-    catch(err){
-      console.error(chalk.red("Error in ReloadTerminal : ", err))
+            await require(`./${commandName}Copy.js`);
+            console.log(chalk.green("loaded file copy "));
+          await delete require.cache[require.resolve(`./${commandName}Copy.js`)];
+          console.log(chalk.green("Unloaded file copy "));
+          await fs.unlink(`./commands/${commandName}Copy.js`, err=>{ //This is the proof that windows sucks. Unlink only delete in linux.
+            console.error(chalk.bgRed("Error : ", err));
+          });
+          console.log(chalk.green("deleted copy file"));
     }
-  },*/
+    catch(err){
+      return console.error(chalk.bgRed("Error in CheckModule, Reload : ", err));
+    }
+}
