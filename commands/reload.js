80f2bc4cfef33de09dd4ce1bebe9e235f71fbee0
Maxime paulin <fanzzy@live.fr>
2019-04-14 14:16:41 +0200

    quick fix

--- commands/reload.js
+++ commands/reload.js
@@ -48,16 +48,16 @@ async function CheckModule(commandName){
       await fs.copyFile(`./commands/${commandName}.js`, `./commands/${commandName}Copy.js`, (err)=>{
       if (err) console.error(chalk.bgRed("Error while copying : ",err));
     });
-      console.log(chalk.green(`${commandName} was copied to ${commandName}Copy`));
+      //console.log(chalk.green(`${commandName} was copied to ${commandName}Copy`));
 
             await require(`./${commandName}Copy.js`);
-            console.log(chalk.green("loaded file copy "));
+          //  console.log(chalk.green("loaded file copy "));
           await delete require.cache[require.resolve(`./${commandName}Copy.js`)];
-          console.log(chalk.green("Unloaded file copy "));
+        //  console.log(chalk.green("Unloaded file copy "));
           await fs.unlink(`./commands/${commandName}Copy.js`, err=>{ //This is the proof that windows sucks. Unlink only delete in linux.
             if (err) return console.error(chalk.bgRed("Error : ", err));
           });
-          console.log(chalk.green("deleted copy file"));
+          //console.log(chalk.green("deleted copy file"));
     }
     catch(err){
       return console.error(chalk.bgRed("Error in CheckModule, Reload : ", err));
--- commands/uptime.js
+++ commands/uptime.js
@@ -6,9 +6,10 @@ exports.run = async (message, client, args, terminal, clientCommands) => {
 
     let totalSeconds = (client.uptime / 1000);
     let days = Math.floor(totalSeconds / 86400);
-    let hours = Math.floor(totalSeconds / 3600);
+    let hours = Math.floor(totalSeconds % 3600);
+    totalSeconds %= 3600;
     let minutes = Math.floor(totalSeconds / 60);
-    let seconds = Math.floor(totalSeconds / 60);
+    let seconds = Math.floor(totalSeconds % 60);
 
       if(!terminal){
         await message.channel.send(`${days} days, ${hours} hours, ${minutes} minutes and ${seconds} seconds since last error.`);
